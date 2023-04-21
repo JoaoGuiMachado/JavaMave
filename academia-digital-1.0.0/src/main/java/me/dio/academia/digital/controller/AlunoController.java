582532642871d5d0c73aaa924fe600f6191732f6
JoaoGuiMachado <joao.gmachado14@gmail.com>
2023-04-21 14:00:18 -0300

    Spring Data JPA

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,31 @@
 package me.dio.academia.digital.controller;
 
+import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import me.dio.academia.digital.entity.form.AlunoForm;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoServicoImpl service;
+
+    @GetMapping
+
+    public List<Aluno> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Aluno create(@RequestBody AlunoForm form) {
+        return service.create(form);
+    }
+
 }
--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoServicoImpl.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoServicoImpl.java
@@ -2,18 +2,32 @@ package me.dio.academia.digital.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.form.AlunoForm;
 import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.service.IAlunoService;
+import net.bytebuddy.asm.Advice.AllArguments;
 
 @Service
 public class AlunoServicoImpl implements IAlunoService{
 
+    @Autowired
+    private AlunoRepository repository; 
+
     @Override
-    public Aluno create(AlunoForm form) {
+    public me.dio.academia.digital.controller.Aluno create(AlunoForm form) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(form.getNome());
+        aluno.setCpf(form.getCpf());
+        aluno.setBairro(form.getBairro());
+        aluno.setDataDeNascimento(form.getDataDeNascimento());
+
+        returnrepository.save(aluno);
+        
         return null;
     }
 
@@ -24,7 +38,7 @@ public class AlunoServicoImpl implements IAlunoService{
 
     @Override
     public List<Aluno> getAll() {
-        return null;
+        return repository.findALL();
     }
 
     @Override
